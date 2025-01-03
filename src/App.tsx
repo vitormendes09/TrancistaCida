73302c6b9147c5bedfe88498b8ff0d9dd9dae3ec
vitormendes09 <mendes.v@gsuite.iff.edu.br>
2025-01-03 19:03:49 -0300

    Atualizando informações da página

--- src/App.tsx
+++ src/App.tsx
@@ -22,33 +22,33 @@ export default function App() {
 
 
                 <div className="">
-                      <h1 className="text-2xl font-medium ">Tranças de Rainha </h1>
+                      <h1 className="text-2xl font-medium text-white ">Tranças de Rainha </h1>
                 </div>
             </div>
 
             <div className="">
                 <button onClick={() => window.location.href = "https://maps.app.goo.gl/LCoLnD7nMuGYDC439"} className="bg-gray-200 p-4 rounded-lg shadow-lg mt-4 flex flex-col items-center justify-center relative hover:scale-105 duration-500 w-full">
-                    <span className="text-xl font-medium"> ATENDIMENTO A DOMICILIO - BOM JESUS</span>
+                    <span className="text-xl font-medium font-mono"> ATENDIMENTO A DOMICILIO - BOM JESUS</span>
                 </button>
                 
-                <button className="w-full">
+                <button className="w-full font-mono">
                     <Popup />
                   
                 </button>
 
                
                 <button onClick={() => window.location.href = "https://www.instagram.com/braidsofqueen/"} className="bg-gray-200 p-4 rounded-lg shadow-lg mt-4 flex flex-col items-center justify-center relative hover:scale-105 duration-500 w-full">
-                    <span className="text-xl font-medium"> INSTAGRAM</span>
+                    <span className="text-xl font-medium font-mono"> INSTAGRAM</span>
                 </button>
 
                 <button onClick={(): string => window.location.href = "https://wa.me/5521993267980"} className="bg-gray-200 p-4 rounded-lg shadow-lg mt-4 flex flex-col items-center justify-center relative hover:scale-105 duration-500 w-full">
-                    <span className="text-xl font-medium"> CONTATO </span>
+                    <span className="text-xl font-medium font-mono"> CONTATO </span>
                 </button>
 
             </div>
             <div className="flex flex-col items-center justify-center absolute inset-x-0 bottom-0 h-6 bg-gray-200">
               <footer className="absolute buttom-0 ">
-                <p> © 2025 Cida| Trancista de Rainha </p>
+                <p> © 2025 Cida| Trancista de Rainhas </p>
               </footer>
 
             </div>
